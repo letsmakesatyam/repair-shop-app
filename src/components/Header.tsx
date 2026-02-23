c4c21db8257f2f7b88e0a8468643c3bba9b2dca0
Satyam Revgade <satyamrevgadelinkedin@gmail.com>
2026-02-23 23:35:31 +0530

    added dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { HomeIcon, File, UsersRound } from "lucide-react"
 import Link from 'next/link'
 import NavButton from '@/components/NabButton'
+import { ModeToggle } from '@/components/ModeToggle'
 export default function Header() {
     return (
         <div className='flex items-center justify-between'>
@@ -10,6 +11,8 @@ export default function Header() {
                 <NavButton Icon={HomeIcon} label="Home" href="/home" />
                 <NavButton Icon={File} label="Tickets" href="/tickets" />
                 <NavButton Icon={UsersRound} label="Customers" href="/customers" />
+                <ModeToggle />
+
             </div>
         </div>
     )
